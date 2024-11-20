9fc0e525d2b7fdd0205094afe7391cd5f18029a1
Kirill-Erofeev <99729641+Kirill-Erofeev@users.noreply.github.com>
2024-11-20 05:26:27 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -36,6 +36,9 @@ class GoldAppleParser():
 
 
     async def get_product_count(self, session: ClientSession, url: str) -> None:
+        '''
+        Функция, предназначенная для нахождения количества товаров по каждому разделу сайта.
+        '''
         async with session.get(url) as response:
             html = await response.text()
             bs = bs4.BeautifulSoup(html, 'html.parser')
@@ -43,11 +46,12 @@ class GoldAppleParser():
                 self.urls[url] = int(bs.find('span', class_='NZnR3')['data-category-products-count'])
             except TypeError:
                 pass
-        # for k, v in self.urls.items():
-        #     print(k, '    ', v)
 
 
     async def run_product(self) -> None:
+        ''''
+        Данная функция запускает вышеуказанную функцию.
+        '''
         async with aiohttp.ClientSession() as session:
             await asyncio.gather(*[
                 self.get_product_count(session, url)
@@ -66,8 +70,6 @@ class GoldAppleParser():
             try:
                 html = await response.text()
                 bs = bs4.BeautifulSoup(html, 'html.parser')
-                # if bs.find('title').text == 'упс... страница не найдена':
-                #     print('exit')
                 products = bs.find_all('a', class_='TTjPn Drqwh QUKnI')
                 for product in products:
                     href = product['href']
@@ -75,7 +77,6 @@ class GoldAppleParser():
                     name = product.find('span', class_='KkVNn').text.strip()
                     brand = product.find('span', class_='Padcv').text.strip()
                     type_ = product.find('div', class_='_7uTPQ').text.strip()
-                    # photos = list(map(lambda x: x['srcset'], product.find_all('source')))
                     photo = product.find('img')['src']
                     price_1 = int(
                         re.sub(
@@ -88,7 +89,6 @@ class GoldAppleParser():
                     in_stock = True if (
                         product.find_all('meta')[-1]['itemprop'] == 'availability'
                     ) else False
-                    # if price_1 == price_2:
                     with jsonlines.open('output.jsonl', mode='a') as writer:
                         writer.write(
                             {   
@@ -112,18 +112,13 @@ class GoldAppleParser():
         '''
         urls = []
 
-        # for url in self.urls:
-        #     urls.extend([f'{url}?p={url_num}' for url_num in range(100)])
         for url, total_product_count in self.urls.items():
             urls.extend(
                 [f'{url}?p={url_num}' for url_num in range(2, total_product_count // self.page_product_count)]
             )
         
-        # urls = ['https://goldapple.ru/lajm?p=5', 'https://goldapple.ru/aptechnaja-kosmetika?p=5']
 
         random.shuffle(urls)
-        # urls = urls[:3000]
-        # print(urls[:11])
         
         async with aiohttp.ClientSession() as session:
             await asyncio.gather(*[
@@ -131,12 +126,6 @@ class GoldAppleParser():
                 for url in urls
             ])
 
-            # for page_num in range(1, 3):
-                # async with session.get(f'https://goldapple.ru/lajm/uhod?p={page_num}') as response:
-
-
-
-gap = GoldAppleParser(24)
-# asyncio.run(gap.run_product())
-asyncio.run(gap.main())
-
+if __name__ == 'main':
+    gap = GoldAppleParser(24)
+    asyncio.run(gap.main())
